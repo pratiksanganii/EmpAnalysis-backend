a92fa9dbd49bace30ab19949caf7cf3ae378e1ee
Pratik Sangani <97470054+pratiksanganii@users.noreply.github.com>
2024-02-17 02:06:54 +0530

    cors added for dev mode

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = require('./src/routes');
 const { errorHandler } = require('./src/error');
 const { PORT } = require('./config');
+const cors = require('cors');
 
 function initializeApp() {
   try {
     const app = express();
+    if (process.env.NODE_ENV == 'development') app.use(cors());
     app.use(express.json());
     app.use('/api', router);
     app.use(errorHandler);
